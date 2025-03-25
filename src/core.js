e9fbed3ea1fdab37c8009920bec51e7735cd83c8
c44rson <ccabre11@lion.lmu.edu>
2025-03-25 13:45:56 -0700

    first attempt at core
    mostly transferred from Carlos notes.
    
    Note that there are many features that still need to be added to Ohm

---
+++ src/core.js
@@ -0,0 +1,141 @@
+export const booleanType = "bool";
+export const intType = "int";
+export const floatType = "float";
+export const stringType = "str";
+export const voidType = "void";
+export const noneType = "none";
+export const anyType = "any";
+
+export function program(statements) {
+  return { kind: "Program", statements };
+}
+
+export function variableDeclaration(variable, initializer) {
+  return { kind: "VariableDeclaration", variable, initializer };
+}
+
+export function variable(name, type) {
+  return { kind: "Variable", name, type };
+}
+
+export function structType(name, fields) {
+  return { kind: "StructType", name, fields };
+}
+
+export function field(name, type) {
+  return { kind: "Field", name, type };
+}
+
+export function fun(name, params, body, type) {
+  return { kind: "Function", name, params, body, type };
+}
+
+export function intrinsicFunction(name, type) {
+  return { kind: "Function", name, type, intrinsic: true };
+}
+
+export function arrayType(baseType) {
+  return { kind: "ArrayType", baseType };
+}
+
+export function functionType(paramTypes, returnType) {
+  return { kind: "FunctionType", paramTypes, returnType };
+}
+
+export function optionalType(baseType) {
+  return { kind: "OptionalType", baseType };
+}
+
+export function assignment(target, source) {
+  return { kind: "Assignment", target, source };
+}
+
+export function returnStatement(expression) {
+  return { kind: "ReturnStatement", expression };
+}
+
+export const shortReturnStatement = { kind: "ShortReturnStatement" };
+
+export function ifStatement(test, consequent, alternate) {
+  return { kind: "IfStatement", test, consequent, alternate };
+}
+
+export function shortIfStatement(test, consequent) {
+  return { kind: "ShortIfStatement", test, consequent };
+}
+
+export function whileStatement(test, body) {
+  return { kind: "WhileStatement", test, body };
+}
+
+export function forStatement(iterator, collection, body) {
+  return { kind: "ForStatement", iterator, collection, body };
+}
+
+export function conditional(test, consequent, alternate, type) {
+  return { kind: "Conditional", test, consequent, alternate, type };
+}
+
+export function unary(op, operand, type) {
+  return { kind: "UnaryExpression", op, operand, type };
+}
+
+export function binary(op, left, right, type) {
+  return { kind: "BinaryExpression", op, left, right, type };
+}
+
+export function emptyOptional(baseType) {
+  return { kind: "EmptyOptional", baseType, type: optionalType(baseType) };
+}
+
+export function arrayExpression(elements) {
+  return {
+    kind: "ArrayExpression",
+    elements,
+    type: arrayType(elements[0].type),
+  };
+}
+
+export function emptyArray(type) {
+  return { kind: "EmptyArray", type };
+}
+
+export function memberExpression(object, op, field) {
+  return { kind: "MemberExpression", object, op, field, type: field.type };
+}
+
+export function functionCall(callee, args) {
+  if (callee.intrinsic) {
+    if (callee.type.returnType === voidType) {
+      return {
+        kind: callee.name.replace(/^\p{L}/u, (c) => c.toUpperCase()),
+        args,
+      };
+    } else if (callee.type.paramTypes.length === 1) {
+      return unary(callee.name, args[0], callee.type.returnType);
+    } else {
+      return binary(callee.name, args[0], args[1], callee.type.returnType);
+    }
+  }
+  return { kind: "FunctionCall", callee, args, type: callee.type.returnType };
+}
+
+export function constructorCall(callee, args) {
+  return { kind: "ConstructorCall", callee, args, type: callee };
+}
+
+const anyToVoidType = functionType([anyType], voidType);
+
+export const standardLibrary = Object.freeze({
+  int: intType,
+  float: floatType,
+  boolean: booleanType,
+  string: stringType,
+  void: voidType,
+  print: intrinsicFunction("print", anyToVoidType),
+});
+
+String.prototype.type = stringType;
+Number.prototype.type = floatType;
+BigInt.prototype.type = intType;
+Boolean.prototype.type = booleanType;
