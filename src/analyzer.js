c85f0012829cfa79ee6e20c9a966ab0b04b5b30c
c44rson <ccabre11@lion.lmu.edu>
2025-03-27 01:41:23 -0700

    analyzer skeleton

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,3 +1,50 @@
-export default function analyze() {
-  throw new Error("Not yet implemented");
+import * as core from "./core.js";
+
+class Context {
+  constructor({
+    parent = null,
+    locals = new Map(),
+    inLoop = false,
+    class: c = null,
+    function: f = null,
+  }) {
+    Object.assign(this, { parent, locals, inLoop, class: c, function: f });
+  }
+  add(name, entity) {
+    this.locals.set(name, entity);
+  }
+  lookup(name) {
+    return this.locals.get(name) || this.parent?.lookup(name);
+  }
+  static root() {
+    return new Context({
+      locals: new Map(Object.entries(core.standardLibrary)),
+    });
+  }
+  newChildContext(props) {
+    return new Context({ ...this, ...props, parent: this, locals: new Map() });
+  }
+}
+
+export default function analyze(match) {
+  /* a local variable to hold the current context */
+  let context = Context.root();
+
+  /* a bunch of semantic validation functions */
+  function must(condition, message, errorLocation) {
+    if (!condition) {
+      const prefix = errorLocation.at.source.getLineAndColumnMessage();
+      throw new Error(`${prefix}${message}`);
+    }
+  }
+
+  function mustNotAlreadyBeDeclared(name, at) {
+    must(!context.lookup(name), `Identifier ${name} already declared`, at);
+  }
+
+  function mustHaveBeenFound(entity, name, at) {
+    must(entity, `Identifier ${name} not declared`, at);
+  }
+  /* Definitions of the semantic actions */
+  /* One line to run it */
 }
