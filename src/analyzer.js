397ef4fa07113b61cb656f5c656d24d602b61d86
c44rson <ccabre11@lion.lmu.edu>
2025-03-28 16:45:20 -0700

    ClassDecl and VarDecl created

--- src/analyzer.js
+++ src/analyzer.js
@@ -11,8 +11,8 @@ class Context {
   }) {
     Object.assign(this, {
       parent,
+      globalClassNamespace,
       locals,
-      attributes,
       inLoop,
       class: c,
       function: f,
@@ -30,15 +30,18 @@ class Context {
   lookup(name) {
     return (
       this.locals.get(name) ||
-      name in this.globalClassNameSpace ||
+      this.globalClassNameSpace?.hasOwnProperty(name) ||
       this.parent?.lookup(name)
     );
   }
   lookupClassName(className) {
-    return className in this.globalClassNamespace;
+    return this.globalClassNameSpace?.hasOwnProperty(className);
   }
   lookupClassItem(className, id) {
-    return id in this.globalClassNamespace[className];
+    return (
+      !!this.globalClassNamespace?.[className] &&
+      id in this.globalClassNamespace[className]
+    );
   }
   static root() {
     return new Context({
@@ -63,171 +66,49 @@ export default function analyze(match) {
   }
 
   // HELPER FUNCTIONS
-  function equivalent(t1, t2) {
-    return (
-      t1 === t2 ||
-      (t1?.kind === "ListType" &&
-        t2?.kind === "ListType" &&
-        equivalent(t1.baseType, t2.baseType)) ||
-      (t1?.kind === "FunctionType" &&
-        t2?.kind === "FunctionType" &&
-        equivalent(t1.returnType, t2.returnType) &&
-        t1.paramTypes.length === t2.paramTypes.length &&
-        t1.paramTypes.every((t, i) => equivalent(t, t2.paramTypes[i])))
-    );
-  }
-
-  function assignable(fromType, toType) {
-    return (
-      toType == core.anyType ||
-      equivalent(fromType, toType) ||
-      (fromType?.kind === "FunctionType" &&
-        toType?.kind === "FunctionType" &&
-        // covariant in return types
-        assignable(fromType.returnType, toType.returnType) &&
-        fromType.paramTypes.length === toType.paramTypes.length &&
-        // contravariant in parameter types
-        toType.paramTypes.every((t, i) =>
-          assignable(t, fromType.paramTypes[i])
-        ))
-    );
-  }
-
-  function typeDescription(type) {
-    if (typeof type === "string") return type;
-    if (type.kind == "ClassType") return type.name;
-    if (type.kind == "FunctionType") {
-      const paramTypes = type.paramTypes.map(typeDescription).join(", ");
-      const returnType = typeDescription(type.returnType);
-      return `(${paramTypes})->${returnType}`;
-    }
-    if (type.kind == "ListType") return `[${typeDescription(type.baseType)}]`;
-  }
-
-  function isMutable(e) {
-    return (
-      (e?.kind === "Variable" && e?.readonly) ||
-      (e?.kind === "SubscriptExpression" && isMutable(e?.list)) ||
-      (e?.kind === "MemberExpression" && isMutable(e?.object))
-    );
-  }
 
   // MUST RULES
-  function mustBeAssignable(e, { toType: type }, at) {
-    const source = typeDescription(e.type);
-    const target = typeDescription(type);
-    const message = `Cannot assign a ${source} to a ${target}`;
-    must(assignable(e.type, type), message, at);
-  }
-
   function mustNotAlreadyBeDeclared(name, at) {
     must(!context.lookup(name), `Identifier ${name} already declared`, at);
   }
 
-  function mustHaveBeenFound(entity, name, at) {
-    must(entity, `Identifier ${name} not declared`, at);
-  }
-
-  function mustHaveNumericType(e, at) {
-    const expectedTypes = [core.intType, core.floatType];
-    must(expectedTypes.includes(e.type), "Expected a number", at);
-  }
-
-  function mustHaveNumericOrStringType(e, at) {
-    const expectedTypes = [core.intType, core.floatType, core.stringType];
-    must(expectedTypes.includes(e.type), "Expected a number or string", at);
-  }
-
-  function mustHaveBooleanType(e, at) {
-    must(e.type === core.booleanType, "Expected a boolean", at);
-  }
-
-  function mustHaveIntegerType(e, at) {
-    must(e.type === core.intType, "Expected an integer", at);
-  }
-
-  function mustBothHaveTheSameType(e1, e2, at) {
-    must(
-      equivalent(e1.type, e2.type),
-      "Operands do not have the same type",
-      at
-    );
-  }
-
-  function mustBeMutable(e, at) {
-    must(isMutable(e), "Cannot assign to immutable variable", at);
-  }
-
-  function mustHaveDistinctFields(type, at) {
-    const fieldNames = new Set(type.fields.map((f) => f.name));
-    must(fieldNames.size === type.fields.length, "Fields must be distinct", at);
-  }
-
-  function mustHaveMember(classType, field, at) {
-    must(
-      classType.fields.map((f) => f.name).includes(field),
-      "No such field",
-      at
-    );
-  }
-
-  function mustBeInLoop(at) {
-    must(context.inLoop, "Break can only appear in a loop", at);
-  }
-
-  function mustBeInAFunction(at) {
-    must(context.function, "Return can only appear in a function", at);
-  }
-
-  function mustBeCallable(e, at) {
-    const callable = e?.kind === "ClassType" || e.type?.kind === "FunctionType";
-    must(callable, "Call of non-function or non-constructor", at);
-  }
-
-  function mustNotReturnAnything(f, at) {
-    const returnsNothing = f.type.returnType === core.voidType;
-    must(returnsNothing, "Something should be returned", at);
-  }
-
-  function mustReturnSomething(f, at) {
-    const returnsSomething = f.type.returnType !== core.voidType;
-    must(returnsSomething, "Cannot return a value from this function", at);
-  }
-
-  function mustBeReturnable(e, { from: f }, at) {
-    mustBeAssignable(e, { toType: f.type.returnType }, at);
-  }
-
-  function mustHaveCorrectArgumentCount(argCount, paramCount, at) {
-    const message = `${paramCount} argument(s) required but ${argCount} passed`;
-    must(argCount === paramCount, message, at);
-  }
   const builder = match.matcher.grammar.createSemantics().addOperation("rep", {
     Program(statements) {
       return core.program(statements.children.map((s) => s.rep()));
     },
 
-    /* Definitions of the semantic actions */
-    VarDecl(modifier, identifier, _colon, Type, _eq, exp) {
-      mustNotAlreadyBeDeclared(identifier.sourceString, { at: identifier });
-      const initializer = exp.rep();
-      const readonly = modifier.sourceString === "readonly";
-      const variable = core.variable(identifier.sourceString, readonly, Type);
-      context.add(identifier.sourceString, variable);
-      return core.variableDeclaration(variable, initializer);
+    Block(_open, statements, _close) {
+      // No need for a block node, just return the list of statements
+      return statements.children.map((s) => s.rep());
     },
 
-    ClassDecl(_class, identifier, _open, constructor, _close) {
-      mustNotAlreadyBeDeclared(identifier.sourceString, { at: identifier });
-      // To allow recursion, enter into context without any fields yet
-      const type = core.structType(identifier.sourceString, []);
-      context.add(identifier.sourceString, type);
-      // Now add the types as you parse and analyze. Since we already added
-      // the struct type itself into the context, we can use it in fields.
-      type.fields = fields.children.map((field) => field.rep());
-      mustHaveDistinctFields(type, { at: identifier });
-      mustNotBeSelfContaining(type, { at: identifier });
-      return core.typeDeclaration(type);
+    /* Definitions of the semantic actions */
+    ClassDecl(_class, id, block) {
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id });
+      const name = id.sourceString;
+      const type = core.classType(name, null, []);
+      context.add(name, type);
+
+      // Create new child context (recursion)
+      context = context.newChildContext({ inLoop: false, class: name });
+
+      type.body = block.rep();
+
+      context = context.parent;
+
+      return core.classDeclaration(type);
+    },
+
+    VarDecl(modifier, _this, _period, id, _colon, type, _eq, exp) {
+      mustNotAlreadyBeDeclared(id.sourceString, { at: id });
+      const readonly = modifier.sourceString === "readonly";
+      const classAffil = context.class; // FIX LATER
+      const name = id.sourceString;
+      const initializer = exp.rep();
+      const variable = core.variable(readonly, classAffil, name, type);
+      context.add(id.sourceString, variable);
+      context.assignClassNamespace(classAffil, name);
+      return core.variableDeclaration(variable, initializer);
     },
   });
   /* One line to run it */
